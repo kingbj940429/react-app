6a8e7eee51d59a7a0e7dab78cd92ac19f80e98b1
kingbj0429 <kingbj0429@lunit.io>
2022-11-15 09:33:16 +0900

    fix: fix decscription

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/kingbj940429/react-app",
+    plugins: [
+        "@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator",
+        "@semantic-release/github"
+    ]
+}
